cc9648989813d8a73c6d5f1dec44717b10f89e8c
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-03-15 11:25:45 +0100

    Creato un modello di e-mail tramite Blade

--- laravel-send-email/resources/views/Email/demoEmail.blade.php
+++ laravel-send-email/resources/views/Email/demoEmail.blade.php
@@ -1,4 +1,4 @@
-<x-mail::message>
+{{-- <x-mail::message>
 # Introduction
 
 The body of your message.
@@ -9,4 +9,14 @@ Button Text
 
 Thanks,<br>
 {{ config('app.name') }}
-</x-mail::message>
+</x-mail::message> --}}
+
+@component('mail::message')
+    # {{ $mailData['title'] }}
+    The body of your message.
+    @component('mail::button', ['url' => $mailData['url']])
+        Button Text
+    @endcomponent
+    Thanks,<br>
+    {{ config('app.name') }}
+@endcomponent
